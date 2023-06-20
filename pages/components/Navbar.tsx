4501f63f6ff1ce46a7a864ad688d0c1765bbf19e
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-06-20 20:52:32 +0200

    test - nav

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,47 +1,58 @@
-import { Roboto_Serif } from "next/font/google";
 import Link from "next/link";
-
-const roboto = Roboto_Serif({
-  weight: "300",
-  style: "normal",
-  subsets: ["latin"],
-});
+import { useState } from "react";
 
 function Navbar() {
-  const redirectTo = () => {
-    console.log("r");
-  };
+  const [isOpen, setIsOpen] = useState(false);
+
 
   return (
-    <nav className={`flex justify-between px-5 py-2 ${roboto.className}`}>
-      <Link
-        className={`cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-pink-400 to-violet-700`}
-        href="/"
-      >
-        kijmoshi
-      </Link>
+    <>
+      <nav className={`p-2 pr-3 h-full flex flex-row justify-between`}>
 
-      <div className={"flex justify-between space-x-10"}>
         <Link
-          className={`cursor-pointer hover:bg-clip-text  hover:text-transparent hover:bg-gradient-to-r from-pink-400 to-violet-700`}
-          href="/projects"
+          href="/"
+          className={`text-white cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-400 hover:to-violet-700`}
         >
-          Projects
+          kijmoshi.xyz
         </Link>
-        <Link
-          className={`cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-pink-400 to-violet-700`}
-          href="/aboutme"
-        >
-          About me
-        </Link>
-        <Link
-          className={`cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r from-pink-400 to-violet-700`}
-          href="/contact"
-        >
-          Contact
-        </Link>
-      </div>
-    </nav>
+
+        <div className={`flex flex-row space-x-3 sm:hidden`}>
+          <Link
+            href="/"
+            className={`text-white cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-400 hover:to-violet-700`}
+          >
+            Projects
+          </Link>
+
+          <Link
+            href="/aboutme"
+            className={`text-white cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-400 hover:to-violet-700`}
+          >
+            About me
+          </Link>
+
+          <Link
+            href="/contact"
+            className={`text-white cursor-pointer hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-pink-400 hover:to-violet-700`}
+          >
+            Contact
+          </Link>
+        </div>
+
+        <div className={`hidden sm:flex flex-row space-x-3`}>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            a
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-6 w-6 text-white`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            />
+          </button>
+        </div>
+      </nav>
+    </>
   );
 }
 
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 function Contact() {
-    const router = useRouter()
+  const router = useRouter();
   return (
     <>
       <h1 className={`text-4xl text-center`}>How to contact me</h1>
@@ -14,21 +14,27 @@ function Contact() {
       <div className={`flex flex-row justify-center mt-20 space-x-6 flex-wrap`}>
         <div
           className={`mt-3 bg-[#5865F2] hover:bg-[#4e5bd9] cursor-pointer w-40 p-1 px-3 flex flex-row items-center rounded`}
-          onClick={() => {router.push("https://discordapp.com/users/617418260484194413")}}
+          onClick={() => {
+            router.push("https://discordapp.com/users/617418260484194413");
+          }}
         >
           <Image src={discord} alt="dc" className={`w-10 h-10 mr-3`} />
           @kijmoshi
         </div>
         <div
           className={`mt-3 bg-white hover:bg-[#DCDCDC] text-black cursor-pointer w-fit w-40 p-1 px-3 flex flex-row items-center rounded`}
-          onClick={() => {router.push("https://github.com/real-kijmoshi")}}
+          onClick={() => {
+            router.push("https://github.com/real-kijmoshi");
+          }}
         >
           <Image src={github} alt="gh" className={`w-10 h-10 mr-3`} />
           real-kijmoshi
         </div>
         <div
           className={`mt-3 bg-[#1DA1F2] hover:bg-[#1DA1F2] cursor-pointer w-fit w-40 p-1 px-3 flex flex-row items-center rounded`}
-          onClick={() => {router.push("https://twitter.com/kijmoshi_dev")}}
+          onClick={() => {
+            router.push("https://twitter.com/kijmoshi_dev");
+          }}
         >
           <Image src={twitter} alt="tt" className={`w-12 h-10 mr-3`} />
           kijmoshi_dev
